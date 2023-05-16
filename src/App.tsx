b3b23ff6a7769bd114d620bd07eb5417c72b05c2
Fabio Carpinato <fabiocarpinato@outlook.it>
2023-05-16 22:18:10 +0200

    feat: adds eslint and ports the missing typescript parts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-function App() {
+import React from 'react'
+
+const App: React.FC = () => {
   return (
     <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
-  );
+  )
 }
 
-export default App;
+export default App
